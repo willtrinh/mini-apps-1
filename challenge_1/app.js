1e79e6b50c9c185ef31e660e21366cb6498c2c5a
Will Trinh <williamltrinh@gmail.com>
2021-04-05 14:05:48 -0700

    Implemented alternate between X and O logic

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -8,26 +8,46 @@
 */
 
 // Array.from will turn the document array into javascript-like array
-const grids = Array.from(document.getElementsByClassName('grid'));
-console.log(grids);
+const squares = Array.from(document.getElementsByClassName('square'));
+// store already placed moves in an array to check for invalid user's click
+const square = [false, false, false, false, false, false, false, false, false];
+const player1 = 'X';
+const player2 = 'O';
+
+// initial current player is X
+let currentPlayer = 'X';
 
 
 const renderBoard = () => {
   // for each grid add vertical and/or horizontal class to draw each grid border
-  grids.forEach((grid, index) => {
+  squares.forEach((square, index) => {
     if (index === 1 || index === 7 || index === 4) {
-      grid.classList.add('vertical');
+      square.classList.add('vertical');
     }
     if (index > 2 && index < 6) {
-      grid.classList.add('horizontal');
+      square.classList.add('horizontal');
     }
-    // add an event listener for each grid clicked
-    grid.addEventListener('click', onGridClick);
+    // add an event listener for each square clicked
+    square.addEventListener('click', onSquareClick);
   })
 }
 
-const onGridClick = (e) => {
-  console.log('grid clicked');
+const onSquareClick = (e) => {
+  // get the square's id when the user clicks the specific square
+  const id = e.target.id;
+  // console.log(`squareId ${squareId} clicked`);
+  // check if the square is already occupied
+  if (!square[id]) {
+    // if not, set the square[id] to the currentPlayer sign
+    square[id] = currentPlayer;
+    // update the currentPlayer to the other sign, X -> O or O -> X
+    e.target.innerText = currentPlayer;
+    if (currentPlayer === player1) {
+      currentPlayer = player2;
+    } else if (currentPlayer === player2) {
+      currentPlayer = player1;
+    }
+  }
 }
 
 renderBoard();
