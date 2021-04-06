bf5769b76ad40ff2546d76047c5f1f0d9357085d
Will Trinh <williamltrinh@gmail.com>
2021-04-06 15:03:19 -0700

    Assignment Complete (baremins)

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -12,29 +12,58 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('client'));
 
-
-app.post('/converter', (req, res) => {
-  var parsedData = JSON.parse(req.body.data);
-  var keysArr = Object.keys(parsedData).join(',');
-  // console.log(keysArr);
-  getChildren(parsedData);
-  res.end();
-});
+// convert json data to csv
+const jsonToCsv = (obj) => {
+  fs.appendFile('./data/result.csv', obj.join(',') + '\n', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
 
 // Get children data of parsed data
 const getChildren = (obj) => {
   var children = Object.values(obj);
-  children.splice(children.length - 1, 1);
-  console.log(children);
+  // remove the nested children array
+  children.splice(children.length - 1);
+  jsonToCsv(children);
   // recursively get nested data
   for (child of obj.children) {
     getChildren(child);
   }
-
 }
 
+// post requests handler
+app.post('/', (req, res) => {
+  var parsedData = JSON.parse(req.body.data);
+  var keysArr = Object.keys(parsedData);
+  // remove the children element from array
+  keysArr.pop();
+  // add json data keys
+  fs.writeFile('./data/result.csv', keysArr.join(',') + '\n', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  })
 
+  // get nested data
+  getChildren(parsedData);
 
+  // send file back to the user to download
+  var filePath = { root: path.join(__dirname, './data') };
+  var file = 'result.csv';
+  fs.readFile('./data/result.csv', 'utf8', (err) => {
+    if (err) {
+      console.log('can\'t read file. ', err);
+    } else {
+      res.sendFile(file, filePath, (err) => {
+        if (err) {
+          console.log('unable to send csv file. ', err);
+        }
+      });
+    }
+  })
+});
 
 
 app.listen(port, () => console.log(`server is listening on port ${port}`));
