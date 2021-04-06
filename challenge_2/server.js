6430938bcde37f9f0cbd38644f31d2ef1fd81520
Will Trinh <williamltrinh@gmail.com>
2021-04-06 15:29:35 -0700

    added file upload (wip)

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Promise = require('bluebird');
 const fs = require('fs');
+const formidable = require('formidable');
 const path = require('path');
 const app = express();
 const port = 9000;
