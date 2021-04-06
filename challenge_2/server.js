c52aecb9268b6b1055b3492ed72da725af4d979b
Will Trinh <williamltrinh@gmail.com>
2021-04-06 11:44:20 -0700

    able to parse json data

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,11 +5,21 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 9000;
-
+// This object will contain key-value pairs,
+//where the value can be a string or array (when extended is false),
+// or any type (when extended is true).
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static('client'));
 
-app.post('/', (req, res) => {
-  res.send('POST request sent');
+app.post('/converter', (req, res) => {
+  var parsedData = JSON.parse(req.body.data);
+  console.log(Object.keys(parsedData));
+  res.send('hello');
 })
+
+
+
+
 
 app.listen(port, () => console.log(`server is listening on port ${port}`));
