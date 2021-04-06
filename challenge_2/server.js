2e6d9d0dd039dd7f46d5e6c2b9bc4d4e19294c07
Will Trinh <williamltrinh@gmail.com>
2021-04-06 11:08:37 -0700

    Connected server & client

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Promise = require('bluebird');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 9000;
 
+app.use(express.static('client'));
 
+app.post('/', (req, res) => {
+  res.send('POST request sent');
+})
+
+app.listen(port, () => console.log(`server is listening on port ${port}`));
