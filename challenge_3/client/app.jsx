e3c5c316d2c5b94e29a27ebefaa9266009d9f473
Will Trinh <williamltrinh@gmail.com>
2021-04-07 11:30:14 -0700

    setup constructor for App component

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,6 +1,26 @@
 class App extends React.Component {
-  render() {
-    return (<h1>hello world!</h1>)
+  constructor(props) {
+    super(props);
+    this.state = {
+      next: 0,
+      name: String,
+      email: '',
+      password: '',
+      line1: '',
+      line2: '',
+      city: '',
+      state: '',
+      zipcode: '',
+      phone: '',
+      ccNumber: '',
+      expMonth: '',
+      expYear: '',
+      cvv: ''
+    }
+    this.handleCheckout = this.handleCheckout.bind(this);
+    this.handleNextClick = this.handleNextClick.bind(this);
+    this.handleConfirmation = this.handleConfirmation.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
   }
 }
 
