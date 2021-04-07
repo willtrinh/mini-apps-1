537ae11315198f3f493084ee17e78fa43a3e93b8
Will Trinh <williamltrinh@gmail.com>
2021-04-07 11:20:19 -0700

    set up checkout schema & db connection

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,1 +1,8 @@
-ReactDOM.render(<h1>hello</h1>, document.getElementById('app'));
+class App extends React.Component {
+  render() {
+    return (<h1>hello world!</h1>)
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,8 +1,40 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(express.static('public'));
-
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 app.listen(port, () => console.log(`server is running on port ${port}`));
+
+const checkoutSchema = new mongoose.Schema ({
+  name: String,
+  email: String,
+  password: String,
+  line1: String,
+  line2: String,
+  city: String,
+  state: String,
+  zipcode: String,
+  phone: String,
+  ccNumber: String,
+  expMonth: String,
+  expYear: String,
+  cvv: String
+});
+const User = mongoose.model('User', checkoutSchema);
+
+app.post('/', (req, res) => {
+  var data = new User(req.body);
+  res.status(200);
+  data.save()
+  .then((item) => {
+    res.send('item saved to db');
+  })
+  .catch((err) => {
+    res.status(400).send('unable to save item to db');
+  })
+})
