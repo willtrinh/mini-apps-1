4201fe0780e585208832fb03ea5b142cf74f6ba5
Will Trinh <williamltrinh@gmail.com>
2021-04-07 10:33:07 -0700

    setup basic html, imported react & babel

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,1 @@
-
-
+ReactDOM.render(<h1>hello</h1>, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express();
+const port = 8080;
 
+app.use(express.static('public'));
+
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
